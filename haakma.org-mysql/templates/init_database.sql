02b0ccd26641df64a41fcc9d0895df0190acfbd5
sido <sido@haakma.org>
2017-09-19 07:00:35 +0200

    [FEATURE] updated ports to open template

--- haakma.org-mysql/templates/init_database.sql
+++ haakma.org-mysql/templates/init_database.sql
@@ -1,3 +1,7 @@
-CREATE USER docker;
-CREATE DATABASE docker;
-GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+CREATE USER #user#;
+
+CREATE DATABASE #db1#;
+CREATE DATABASE #db2#;
+
+GRANT ALL PRIVILEGES ON #db1#.* TO '#user#'@'%' IDENTIFIED BY '#password#';
+GRANT ALL PRIVILEGES ON #db2#.* TO '#user#'@'%' IDENTIFIED BY 'password';

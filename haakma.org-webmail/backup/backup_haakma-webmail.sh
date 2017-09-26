ad931c8196d3b6831f3059e1e7562e385dda2881
sido <sido@haakma.org>
2017-09-26 09:45:49 +0200

    [FIX] backup failes when database is inaccessible

--- haakma.org-webmail/backup/backup_haakma-webmail.sh
+++ haakma.org-webmail/backup/backup_haakma-webmail.sh
@@ -19,6 +19,10 @@ echo "***************************************************************"
 
 echo "[INFO] Backup MySQL schema : ${SQL_SCHEMA}"
 mysqldump -u ${SQL_USER} -p${SQL_PASSWORD} ${SQL_SCHEMA} --host=${SQL_HOST} --port=${SQL_PORT}  > ${BASE_DIR}/${DATE}_haakma_mail.sql
+if [[ ${?} != 0 ]]; then
+  echo "[ERROR] mysqldump failed"
+  exit 1
+fi
 
 echo "[INFO] Backup data directory : ${WWW_DIR}/webmail.haakma.org/"
 tar -cvf ${BASE_DIR}/${DATE}_haakma_mail.tar ${WWW_DIR}/webmail.haakma.org/
